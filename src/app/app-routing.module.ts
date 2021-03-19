ea86a0b085fa98d3281bd1a4bf7f796063afd47b
Allan Guerra <allang@ciandt.com>
2021-03-19 00:24:18 -0300

    feat: criando rota para todo-list

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,16 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    loadChildren: () => import('./modules/todo/todo.module').then(m => m.TodoModule)
+  },
+  {
+    path: '**',
+    redirectTo: ''
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
