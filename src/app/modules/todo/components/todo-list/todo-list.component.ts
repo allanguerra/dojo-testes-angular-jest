dd106f4076868c0e74ec574aa7554bd57a70bac5
Allan Guerra <allang@ciandt.com>
2021-03-19 01:27:15 -0300

    feat: finalizando estrutura base de lista de tarefas

--- src/app/modules/todo/components/todo-list/todo-list.component.ts
+++ src/app/modules/todo/components/todo-list/todo-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-todo-list',
@@ -7,9 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TodoListComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private readonly router: Router
+  ) { }
 
   ngOnInit() {
+  }
+
+  public acaoNovaTarefa(): void {
+    this.router.navigate(['new']);
   }
 
 }
