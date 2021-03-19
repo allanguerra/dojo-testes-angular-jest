18fec64adff62d79bbc7c3cc1ef8ce4b4976f6dc
Allan Guerra <allang@ciandt.com>
2021-03-19 01:28:12 -0300

    feat: criando todo task component

---
+++ src/app/shared/components/todo-task/todo-task.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-todo-task',
+  templateUrl: './todo-task.component.html',
+  styleUrls: ['./todo-task.component.scss']
+})
+export class TodoTaskComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,12 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { TodoTaskComponent } from './components/todo-task/todo-task.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    TodoTaskComponent
+  ],
   imports: [
     CommonModule
+  ],
+  exports: [
+    TodoTaskComponent
   ]
 })
 export class SharedModule { }
