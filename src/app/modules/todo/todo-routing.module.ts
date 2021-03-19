d6f01e492a8958ff054782046a62fb9187fb7607
Allan Guerra <allang@ciandt.com>
2021-03-19 01:25:56 -0300

    feat: criando roteamento para todo form

--- src/app/modules/todo/todo-routing.module.ts
+++ src/app/modules/todo/todo-routing.module.ts
@@ -1,8 +1,28 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { TodoFormComponent } from 'src/app/modules/todo/components/todo-form/todo-form.component';
+import { TodoListComponent } from 'src/app/modules/todo/components/todo-list/todo-list.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'todo-list',
+    pathMatch: 'full'
+  },
+  {
+    path: 'todo-list',
+    component: TodoListComponent
+  },
+  {
+    path: 'new',
+    component: TodoFormComponent
+  },
+  {
+    path: 'edit/:id',
+    component: TodoFormComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
