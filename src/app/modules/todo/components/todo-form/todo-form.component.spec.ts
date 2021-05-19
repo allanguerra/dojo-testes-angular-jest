6739c8964c3787bb75e1832795bcdfb18e393b77
Allan Guerra <allang@ciandt.com>
2021-05-19 12:11:49 -0300

    resolvendo todo form component

--- src/app/modules/todo/components/todo-form/todo-form.component.spec.ts
+++ src/app/modules/todo/components/todo-form/todo-form.component.spec.ts
@@ -6,6 +6,9 @@ import { TodoFormComponent } from './todo-form.component';
 import { RouterTestingModule } from '@angular/router/testing';
 import { TodoService } from 'src/app/modules/todo/services/todo-service/todo.service';
 import { ActivatedRoute } from '@angular/router';
+import { Todo } from 'src/app/modules/todo/models/todo.model';
+import { TodoStatus } from 'src/app/modules/todo/enums/todo-status.enum';
+import { of } from 'rxjs';
 
 describe('TodoFormComponent', () => {
   let component: TodoFormComponent;
@@ -23,7 +26,7 @@ describe('TodoFormComponent', () => {
         {
           provide: ActivatedRoute,
           useValue: {
-            snapshot: { url: [{ path: 'any_path' }] }
+            snapshot: { url: [{ path: 'new' }] }
           }
         }
       ],
@@ -41,4 +44,36 @@ describe('TodoFormComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  // 1. descricao do teste
+  it('deve desabilitar o botao salvar quando o form estiver invalido', () => {
+    // 2. fazendo mocks necessarios
+    const serviceSpy = jest.spyOn(TodoService.prototype, 'salvarNovaTarefa')
+      .mockReturnValueOnce( of(fixtureTarefaSalva()) );
+
+    // 3. montando estrutura inicial do teste
+    component.ngOnInit();
+    fixture.detectChanges();
+
+    component.salvando = false;
+    component.todoForm.get('title').setValue(null);
+    fixture.detectChanges();
+
+    // 4. executando cenario
+    const botaoSalvar: HTMLButtonElement = fixture.debugElement.nativeElement.querySelector('#botaoSalvar');
+    botaoSalvar.click();
+    fixture.detectChanges();
+
+    // 5. verificando
+    expect(serviceSpy).not.toHaveBeenCalled();
+  });
 });
+
+function fixtureTarefaSalva(): Todo {
+  return {
+    id: 1,
+    title: 'any_title',
+    description: 'any_description',
+    status: TodoStatus.PENDENTE
+  };
+}
